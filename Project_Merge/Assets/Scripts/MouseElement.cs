a888769aa83e187aebd4b521afebbdcb927df83c
Double_U_ <junhyup511@gmail.com>
2020-09-08 21:48:31 +0900

    -----------09/08 Patch Note----------- 링크를 참조하여 기초적인 머지 알고리즘 개발 # 스프라이트 리스트가 아닌 오브젝트를 이용함 # 코드를 쓰긴했는데 대부분 바꿔놈, TileMaps 삭제 # MouseSpriteManager에 프리팹 리스트 다 모아놈
    쥐 이미지 추가 -> 쥐 프리팹 39개 완성
    # 1반 김○혁 이라는 사람이 40번 쥐 제대로 완성 안해서 39개만 만듬
    # 각각 프리팹에 RigidBody 2D, Box Colider 2D, 그리고 스크립트 MouseElement 있음
    ㄴ 각각 쥐마다 충돌 Box 사이즈 조절 필요
    
    박쥐, 쥐폐 애니메이션 일단 설정해놈
    # 바꾸려면 바꾸던가
    
    싱글톤 추가
    # 아직 쓰진 않음

---
+++ Project_Merge/Assets/Scripts/MouseElement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseElement : MonoBehaviour
+{
+    [SerializeField]
+    private int mouseID = 0;
+
+    bool MoveWithComputerMouse = false;
+    bool CollidedWithOther = false;
+
+    MouseSpriteManager MSM;
+    GameObject triggered;
+
+    void Awake()
+    {
+        MSM = FindObjectOfType<MouseSpriteManager>();
+    }
+
+    void Update()
+    {
+        if (MoveWithComputerMouse && GetComponent<SpriteRenderer>().enabled)
+        {
+            transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        }
+    }
+    void OnMouseDown()
+    {
+        MoveWithComputerMouse = true;
+    }
+    void OnMouseUp()
+    {
+        MoveWithComputerMouse = false;
+        if (CollidedWithOther)
+        {
+            OntoOtherOne();
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.GetComponent<MouseElement>())
+        {
+            triggered = col.gameObject;
+            CollidedWithOther = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.GetComponent<MouseElement>())
+        {
+            CollidedWithOther = false;
+        }
+    }
+
+    void OntoOtherOne()
+    {
+        if (triggered.GetComponent<MouseElement>().mouseID == gameObject.GetComponent<MouseElement>().mouseID)
+        {
+            Merge();
+        }
+    }
+
+    void Merge()
+    {
+        triggered.GetComponent<MouseElement>().UpdateMouseElementSprite();
+        Hide();
+    }
+
+    void Hide()
+    {
+        Destroy(gameObject);
+    }
+
+    void UpdateMouseElementSprite()
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            if (i == 39)
+                break;
+            if(gameObject.GetComponent<MouseElement>().mouseID == MSM.TileSprites[i].GetComponent<MouseElement>().mouseID)
+                Instantiate(MSM.TileSprites[i+1], triggered.transform.localPosition, triggered.transform.localRotation);
+        }
+        if (gameObject.GetComponent<MouseElement>().mouseID != 39)
+            Destroy(gameObject);
+    }
+}
---
+++ Project_Merge/Assets/Scripts/MouseSpriteManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSpriteManager : MonoBehaviour
+{
+    [SerializeField]
+    public List<GameObject> TileSprites = new List<GameObject>();
+}
