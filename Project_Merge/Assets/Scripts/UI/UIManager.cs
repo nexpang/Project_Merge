5aceef31cdfe3b71249dd2ba6e33ab3303b78f7c
KimMinSang <minsang1008@naver.com>
2020-09-21 19:56:28 +0900

    쥐 소환

--- Project_Merge/Assets/Scripts/UI/UIManager.cs
+++ Project_Merge/Assets/Scripts/UI/UIManager.cs
@@ -1,9 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEditor.VersionControl;
+using Microsoft.Win32.SafeHandles;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    private Text Cheese = null;
+    [SerializeField]
+    private Text Money = null;
+    [SerializeField]
+    private int NeedCheese = 0;
+    [SerializeField]
+    private int MCheese = 0;
+    [SerializeField]
+    private int Mmoney = 0;
+    [SerializeField]
+    private int mouseID = 0;
+    [SerializeField]
+    private GameObject mouse = null;
+    [SerializeField]
+    private Transform pposition = null;
+
+    MouseSpriteManager MSM;
+    GameObject triggered;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +39,23 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void SpreadCheese()
+    {
+        if(MCheese > NeedCheese)
+        {
+            MCheese -= NeedCheese;
+            Instantiate(mouse,pposition.position, Quaternion.identity);
+            UpdateMoneyCheese();
+        }
+        else
+        {
+            
+        }
+    }
+    private void UpdateMoneyCheese()
+    {
+        Money.text = Mmoney.ToString();
+        Cheese.text = MCheese.ToString();
     }
 }
