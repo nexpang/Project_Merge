2a45aa311a396fc9c69929072840c38966c5188a
nexpang <rlarudgur123@gmail.com>
2020-10-08 00:44:08 +0900

    Revert "Revert "빌드되게하고 키스톤 추가함""
    This reverts commit d818894a4db16531ebaf9459ab640b9f708f126c.

--- Project_Merge/Assets/Scripts/UI/UIManager.cs
+++ Project_Merge/Assets/Scripts/UI/UIManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEditor.VersionControl;
 using Microsoft.Win32.SafeHandles;
 using DG.Tweening;
 
