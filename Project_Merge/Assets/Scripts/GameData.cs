6fd34833c3757ff7408d38832d80e33ab09c7513
Double_U_ <junhyup511@gmail.com>
2020-10-15 22:32:29 +0900

    자잘버그 수정

--- Project_Merge/Assets/Scripts/GameData.cs
+++ Project_Merge/Assets/Scripts/GameData.cs
@@ -9,7 +9,7 @@ public class GameData
     public long Cheese = 0;
     public long Money = 0;
     public int Upgrade_MoneyElapsedTimeStack = 0;
-    public int Upgrade_CheeseStack = 1;
+    public int Upgrade_CheeseStack = 0;
 
     public List<int> MiceList = new List<int>();
     public List<Vector2> MiceXY = new List<Vector2>();
