771abf2b8c4fd27f4a44e223d6934270205d8cd8
KimMinSang <minsang1008@naver.com>
2020-10-31 05:10:03 +0900

    하단 광고 배너 추가 + 배너 추가에 따른 ui 위치 변경 쥔공청소기 위치 변경 바람

---
+++ Project_Merge/Assets/Scripts/AdmobManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleMobileAds.Api;
+public class AdmobManager : MonoBehaviour
+{
+    public bool isTestMode;
+    //public Text LogText;
+    //public Button FrontAdsBtn, RewardAdsBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBannerAd();
+        //LoadRewardAd();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    AdRequest GetAdRequest()
+    {
+        return new AdRequest.Builder().AddTestDevice("331247D1AFD5E740").Build();
+    }
+    #region 배너광고
+    const string bannerTestID = "ca-app-pub-3940256099942544/6300978111";
+    const string bannerID = "ca-app-pub-4925895011467232/8017914646";
+    BannerView bannerAd;
+    void LoadBannerAd()
+    {
+        bannerAd = new BannerView(isTestMode ? bannerTestID : bannerID, AdSize.SmartBanner, AdPosition.Bottom);
+        bannerAd.LoadAd(GetAdRequest());
+        ToggleBannerAd(true);
+    }
+    public void ToggleBannerAd(bool b)
+    {
+        if (b) bannerAd.Show();
+        else bannerAd.Hide();
+    }
+    #endregion
+}
