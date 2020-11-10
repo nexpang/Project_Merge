18078d5b300427112d1ca37b4e4a747f3e3f98be
KimMinSang <minsang1008@naver.com>
2020-11-10 10:15:18 +0900

    크레딧 대충 해놨음

---
+++ Project_Merge/Assets/Scripts/UI/OnOffManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Button = null;
+    public void ButtonActive()
+    {
+        AudioManager.Instance.ASButtonClick.Play();
+        if (Button.activeSelf)
+        {
+            Button.SetActive(false);
+        }
+        else
+        {
+            Button.SetActive(true);
+        }
+    }
+}
